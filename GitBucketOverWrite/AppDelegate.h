d6ef3c9bfd0b88a14275fe4c033e1bd3def9730a
ZelinDavid <1102533858@qq.com>
2017-07-10 11:56:16 +0800

    Initial Commit

---
+++ GitBucketOverWrite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitBucketOverWrite
+//
+//  Created by David on 17/7/10.
+//  Copyright © 2017年 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitBucketOverWrite/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitBucketOverWrite
+//
+//  Created by David on 17/7/10.
+//  Copyright © 2017年 David. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
